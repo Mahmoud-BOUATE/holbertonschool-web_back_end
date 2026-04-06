ff3f1c693a87922e0ee799a6ab1b03295cc583aa
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-06 10:45:14 +0200

    Refactor to_str function to return string representation of float instead of using math.floor

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Module that takes a float n as argument"""
 
-import math
 
-
-def to_str(n: float) -> str | float:
-    """Return a strings representation of the float """
-    return math.floor(n)
+def to_str(n: float) -> str:
+    """Return a string representation of the float."""
+    return str(n)
