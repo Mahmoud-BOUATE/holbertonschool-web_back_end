8a3dc5fb5033ab93d78ba79779cdaecde331ee65
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-06 16:18:23 +0200

    Refactor import statement formatting in measure_runtime.py for consistency

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,9 +4,9 @@
 import asyncio   # pour exécuter des fonctions async
 import time      # pour mesurer le temps
 
-
 # On importe wait_n depuis ton fichier précédent
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """
