8e66153521be67889ae2f9b05246a142a8d0ebe7
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-06 11:06:52 +0200

    Add sum_list module for calculating the sum of a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that takes a list of loat as arguments and return ther sum"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function that return the sum of a list as float"""
+    return sum(input_list)
