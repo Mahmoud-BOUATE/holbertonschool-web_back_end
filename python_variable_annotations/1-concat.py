dff06e84364cfcf19dfae978adf116a191035f4a
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-06 10:27:09 +0200

    Fix formatting of concat function parameters and add docstring

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,5 +2,6 @@
 """Module that concat the strings"""
 
 
-def concat(str1: str, str2: str) ->str :
+def concat(str1: str, str2: str) -> str:
+    """returns a concatenated string"""
     return str1 + str2
