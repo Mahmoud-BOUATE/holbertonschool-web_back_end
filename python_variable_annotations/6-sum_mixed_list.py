44bcab4c1bc152b3ef851e318cd4a840ff6561f4
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-06 11:42:56 +0200

    Fix docstring in sum_mixed_list module to clarify return type

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-"""Module that takes a list od int and floats and return their sum as a float"""
+"""Module that takes a list od int and floats and return their sum"""
 
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Sum of integers and floats"""
