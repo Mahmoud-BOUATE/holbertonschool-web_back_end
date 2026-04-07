ac434771d2ae24e3fa8981a88ad3ef7ad36b2df2
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-07 12:15:19 +0200

    Refactor task_wait_n function to use task_wait_random and improve docstring clarity; add main script for testing

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-"""
-Docstring for python_async_function.4-tasks
-"""
+"""Let's execute multiple coroutines at the same time with async"""
+
 import asyncio
 from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Launch n tasks using task_wait_random
-    and return their results as a list of floats.
-    """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    """multiple coroutines at the same time with async"""
+    tasks = []
+
+    for _ in range(n):
+        tasks.append(asyncio.create_task(task_wait_random(max_delay)))
+
     results = []
+
     for task in asyncio.as_completed(tasks):
         result = await task
         results.append(result)
+
     return results
