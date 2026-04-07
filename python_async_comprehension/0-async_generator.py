01c5bc281e9e522a11bba34b14a99e8fda8e8299
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-07 17:36:02 +0200

    Refactor async_generator.py to improve function clarity and testing; replace main function with print_yielded_values

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-"""Module Async Generator"""
+"""Test async_generator"""
 
 import asyncio
-import random
 
+# Import de la coroutine async_generator
+async_generator = __import__('0-async_generator').async_generator
 
-async def async_generator():
-    """Function that yield a ranom number between 0 and 10"""
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield(random.uniform(0,10))
+async def print_yielded_values():
+    """Récupère toutes les valeurs yieldées et les affiche"""
+    result = []
+    async for value in async_generator():
+        result.append(value)
+    print(result)
 
-async def main():
-    async for i in async_generator():
-        print(i)
-
-asyncio.run(main())
+# Exécution
+asyncio.run(print_yielded_values())
