75998bcfeae939bd6bb6be51c5556767e94d17a6
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-07 17:41:37 +0200

    Add type hint to async_generator function for improved clarity

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random floats between 0 and 10 with 1-second async waits."""
     for _ in range(10):
         await asyncio.sleep(1)
