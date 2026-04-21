46998ddd1ccd369e23d417a091c37725e796149e
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-21 09:27:21 +0200

    Add initial implementation of index_range function and main file for testing

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module Simple helper function """
+
+
+def index_range(page, page_size):
+    """Simple helper function"""
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    return start, end
