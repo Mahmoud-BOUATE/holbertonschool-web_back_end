455f37af2f1a8b29da39767c4298de7cfcd7d8f7
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-21 09:53:31 +0200

    Refactor module docstring for clarity and remove unnecessary whitespace

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module pagination"""
 
 
 import csv
@@ -12,12 +13,11 @@ def index_range(page, page_size):
 
     return start, end
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
-
-
 
     def __init__(self):
         self.__dataset = None
@@ -40,4 +40,3 @@ class Server:
         data = self.dataset()
         start, end = index_range(page, page_size)
         return data[start:end]
-
