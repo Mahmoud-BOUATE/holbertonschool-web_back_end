040e88d8dc4c29f88409d06a93c3177c18252d88
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-24 17:19:53 +0200

    Refactor Server class methods for improved clarity and consistency

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -2,7 +2,7 @@
 """Deletion-resilient hypermedia pagination."""
 
 import csv
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 
 class Server:
@@ -10,52 +10,45 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
-    def __init__(self):
-        """Initialize cached dataset and indexed dataset."""
-        self.__dataset = None
-        self.__indexed_dataset = None
+    def init(self):
+        self.dataset = None
+        self.indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
         """
-        if self.__dataset is None:
+        if self.dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+            self.dataset = dataset[1:]
 
-        return self.__dataset
+        return self.dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
-        if self.__indexed_dataset is None:
+        if self.indexed_dataset is None:
             dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
 
-            self.__indexed_dataset = {
-                i: truncated_dataset[i] for i in range(len(truncated_dataset))
+            self.indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
             }
-        return self.__indexed_dataset
+        return self.indexed_dataset
 
-    def get_hyper_index(
-        self,
-        index: Optional[int] = None,
-        page_size: int = 10
-    ) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a deletion-resilient page and pagination metadata."""
         dataset = self.indexed_dataset()
         if index is None:
             index = 0
-        max_index = max(dataset.keys()) if dataset else -1
 
-        assert isinstance(index, int) and 0 <= index <= max_index
-        assert isinstance(page_size, int) and page_size > 0
+        assert type(index) is int and 0 <= index < len(self.dataset())
+        assert type(page_size) is int and page_size > 0
 
-        data: List[List] = []
+        data = []
         next_index = index
 
-        while len(data) < page_size and next_index <= max_index:
+        while len(data) < page_size and next_index < len(self.dataset()):
             if next_index in dataset:
                 data.append(dataset[next_index])
             next_index += 1
