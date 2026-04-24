7af8d216ac0665bc2adf6a7408f7b37546e1c911
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-24 14:28:57 +0200

    Add main script and schools_by_topic module for school topic management

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module 11-schools_by_topic
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school documents having a specific topic.
+
+    Args:
+        mongo_collection: pymongo collection object
+        topic (str): topic to search for
+
+    Returns:
+        list of matching school documents
+    """
+    return list(mongo_collection.find({"topics": topic}))
