f34371a7cf64b8f6989e3cf0c4cd4960a34ffd83
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-24 14:19:39 +0200

    Update docstring for insert_school function to clarify parameters and return value

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-"""Insert a document in Python"""
-
+"""
+Module 9-insert_school
+"""
 
 def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs.
+
+    Args:
+        mongo_collection: pymongo collection object
+        **kwargs: fields of the document to insert
+
+    Returns:
+        The new document _id
+    """
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
-
