e4c6c97cee1cb626114a6f3c4f26b76d0ef1dc99
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-29 16:42:50 +0200

    feat: add HolbertonClass and main file for instantiation and testing

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,40 @@
+class HolbertonClass {
+    constructor(size, location) {
+        this.size = size;
+        this.location = location;
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    get location() {
+        return this._location;
+    }
+
+    set size(value) {
+        if (typeof value !== "number") {
+            throw new TypeError("size must be a number");
+        }
+        this._size = value;
+    }
+
+    set location(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("location must be a string");
+        }
+        this._location = value;
+    }
+
+    [Symbol.toPrimitive](hint) {
+        if (hint === "number") {
+            return this._size;
+        }
+        if (hint === "string") {
+            return this._location;
+        }
+        return this._size;
+    }
+}
+
+export default HolbertonClass;
