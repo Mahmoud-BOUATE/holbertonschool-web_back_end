0d305526e1e72673480a32412246fa87adcaa7b8
mahmoud BOUATE <12065@holbertonschool.com>
2026-04-29 13:41:22 +0200

    feat: add Currency class and main file for instantiation and testing

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+    constructor(code, name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // GETTERS
+    get code() {
+        return this._code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    // SETTERS
+    set code(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("Code must be a string");
+        }
+        this._code = value;
+    }
+
+    set name(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("Name must be a string");
+        }
+        this._name = value;
+    }
+
+    // METHOD
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
