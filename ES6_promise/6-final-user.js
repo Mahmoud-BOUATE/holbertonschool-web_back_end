53bfab28bf483a7232e544cd8a4d7b2f13eb27f9
mahmoud BOUATE <12065@holbertonschool.com>
2026-05-06 11:48:04 +0200

    feat: implement handleProfileSignup function and main execution script

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,25 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ])
+    .then((results) => {
+      return results.map((result) => {
+        let value;
+
+        if (result.status === 'fulfilled') {
+          value = result.value;
+        } else {
+          value = result.reason;
+        }
+
+        return {
+          status: result.status,
+          value: value
+        };
+      });
+    });
+}
