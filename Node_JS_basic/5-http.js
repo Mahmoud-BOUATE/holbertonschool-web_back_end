54a84542ea19685d8115e2113d73ea466410116e
mahmoud BOUATE <12065@holbertonschool.com>
2026-05-13 17:02:00 +0200

    fix: improve code formatting and consistency in HTTP server implementation

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,28 +1,28 @@
-const http = require('http')
-const countStudents = require('./3-read_file_async')
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    if (req.url === '/') {
-        res.end('Hello Holberton School!')
-    } else if (req.url === '/students') {
-        const database = process.argv[2]
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const database = process.argv[2];
 
-        res.write('This is the list of our students\n')
+    res.write('This is the list of our students\n');
 
-        countStudents(database)
-            .then(() => {
-                res.end()
-            })
-            .catch(() => {
-                res.end('Cannot load the database')
-            })
-    } else {
-        res.end('Not found')
-    }
-})
+    countStudents(database)
+      .then(() => {
+        res.end();
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.end('Not found');
+  }
+});
 
-app.listen(1245)
+app.listen(1245);
 
-module.exports = app
+module.exports = app;
