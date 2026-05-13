92945364202c49f12ee7618cc378255ce61e3081
mahmoud BOUATE <12065@holbertonschool.com>
2026-05-13 15:47:15 +0200

    fix: comment out deprecated stdin data handling for improved readability

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
-process.stdin.on('data', (data) => {
+/**
+ * process.stdin.on('data', (data) => {
     const name = data.toString().trim()
     process.stdout.write(`Your name is: ${name}\r`)
+})
+ */
+
+process.stdin.on('readable', () => {
+    const name = process.stdin.read()
+    if( name !== null){
+        process.stdout.write(`Your name is ${name}`)
+    }
 })
 
 process.stdin.on('end', () => {
